cd2c8ee2aac1333f2f45b9ec273b432a37c52029
Taeho1218 <true3d.j@gmail.com>
2024-05-12 08:13:58 +0000

    java_practice

---
+++ Study.java
@@ -0,0 +1,9 @@
+public class Study{
+    public static void main (String[] args){
+        String name = "정태호";
+        int age = 23;
+
+        System.out.printf("안녕하세요 저의 이름은 %s 이고 저의 나이는 %d 입니다" ,name,age);
+
+    }
+}
